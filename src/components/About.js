dde270bdbcb699243cef220b424c623982b292aa
Bhagirath Makwana <83909096+Bhagi-developer@users.noreply.github.com>
2021-09-30 13:28:59 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function About(props) {
+  // const [myStyle, setMyStyle] = useState({
+  //     color: 'black',
+  //     backgroundColor: 'white'
+  // })
+  let myStyle = {
+    color: props.mode === "dark" ? "white" : "#042743",
+    backgroundColor: props.mode === "dark" ? "rgb(36 74 104)" : "white",
+  };
+
+  return (
+    <div className="container">
+      <h1
+        className="my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        About Us
+      </h1>
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button
+              className="accordion-button"
+              type="button"
+              style={myStyle}
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+              <strong>About This React App! </strong>
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              TextConvertor gives you an utility to use features Like convert in
+              upper lowercase, Clear your text, cut and copy your text, count
+              words and characters of your text etc.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button
+              className="accordion-button collapsed"
+              style={myStyle}
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              <strong>About Me</strong>
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              {" "}
+              I am an computer engineering student at L.D.College of
+              engineering, Ahmedabad. Currently studing in third year, I am
+              Lokking for a web developement internship in MERN stack.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingThree">
+            <button
+              className="accordion-button collapsed"
+              style={myStyle}
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              <strong>Home </strong>
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              {/* This word counter software works in any web browsers such as
+              Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to
+              count characters in facebook, blog, books, excel document, pdf
+              document, essays, etc. */}
+              <Link to="/">Go Home</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/TextForm.js
@@ -0,0 +1,216 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  let [heading, setHeading] = useState(
+    "TextConverter - Get Your Text Formated!!!"
+  );
+
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to uppercase!", "done");
+
+    setTimeout(() => {
+      setHeading(
+        <div class="text-center">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      );
+    }, 10);
+
+    setTimeout(() => {
+      setHeading("TextConverter - Get Your Text Formated!!!");
+    }, 400);
+  };
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to lowercase!", "done");
+    setTimeout(() => {
+      setHeading(
+        <div class="text-center">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      );
+    }, 10);
+
+    setTimeout(() => {
+      setHeading("TextConverter - Get Your Text Formated!!!");
+    }, 400);
+  };
+
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text Cleared!", "done");
+
+    setTimeout(() => {
+      setHeading(
+        <div class="text-center">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      );
+    }, 10);
+
+    setTimeout(() => {
+      setHeading("TextConverter - Get Your Text Formated!!!");
+    }, 400);
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    props.showAlert("Copied to Clipboard!", "done");
+
+    setTimeout(() => {
+      setHeading(
+        <div class="text-center">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      );
+    }, 10);
+
+    setTimeout(() => {
+      setHeading("TextConverter - Get Your Text Formated!!!");
+    }, 400);
+  };
+
+  const handleExtraSpaces = () => {
+    setText(text.split(/[ ]+/).join(" "));
+    props.showAlert("Extra spaces removed!", "done");
+
+    setTimeout(() => {
+      setHeading(
+        <div class="text-center">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      );
+    }, 10);
+
+    setTimeout(() => {
+      setHeading("TextConverter - Get Your Text Formated!!!");
+    }, 400);
+  };
+
+  const handleCut = () => {
+    navigator.clipboard.writeText(text);
+    setText("");
+    props.showAlert("Copy and Removed!", "done");
+
+    setTimeout(() => {
+      setHeading(
+        <div class="text-center">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      );
+    }, 10);
+
+    setTimeout(() => {
+      setHeading("TextConverter - Get Your Text Formated!!!");
+    }, 400);
+  };
+
+  const [text, setText] = useState("");
+  // text = "new text"; // Wrong way to change the state
+  // setText("new text"); // Correct way to change the state
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        <h1 className="mb-4">{heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "#13466e" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-outline-primary mx-1 my-1"
+          onClick={handleUpClick}
+        >
+          Convert to Uppercase
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-outline-secondary mx-1 my-1"
+          onClick={handleLoClick}
+        >
+          Convert to Lowercase
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn  btn-outline-success mx-1 my-1"
+          onClick={handleClearClick}
+        >
+          Clear Text
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-outline-danger mx-1 my-1"
+          onClick={handleCopy}
+        >
+          Copy Text
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-outline-warning mx-1 my-1"
+          onClick={handleCut}
+        >
+          Copy And Clear
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-outline-info mx-1 my-1"
+          onClick={handleExtraSpaces}
+        >
+          Remove Extra Spaces
+        </button>
+      </div>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        <h2>Summary Of Text</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>
+          Estimates{" "}
+          {0.008 *
+            text.split(/\s+/).filter((element) => {
+              return element.length !== 0;
+            }).length}{" "}
+          Minutes read
+        </p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "you didn't write text yet!"}</p>
+      </div>
+    </>
+  );
+}
